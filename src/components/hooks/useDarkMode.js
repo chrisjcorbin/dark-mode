f53bb34b6101efb68b1ecb0dac24dfc166dd7d41
Chris <cjcorbin@gmail.com>
2020-09-02 12:20:15 -0700

    Code Tidying Up

--- src/components/hooks/useDarkMode.js
+++ src/components/hooks/useDarkMode.js
@@ -1,7 +1,7 @@
 import { useLocalStorage } from "./useLocalStorage";
 
 export const useDarkMode = (initialValue) => {
-    const [value, setValue] = useLocalStorage("darkmode", initialValue);
+    const [value, setValue] = useLocalStorage("darkMode", initialValue);
 
     return [value, setValue];
 };
